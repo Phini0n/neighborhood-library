808f6412e5fdb71e4b2de3a3d99b5ffbf4176f81
Phini0n <patrayowilson@gmail.com>
2024-10-04 15:47:05 -0400

    Initial commit.

---
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class NeighborhoodLibrary {
+    public static void main(String[] args) {
+        Book[] inventory = new Book[20];
+        inventory[0] = new Book("9780786965601", "Player's Handbook. 5th Edition");
+        inventory[1] = new Book("9781338111002", "City of Ghosts");
+
+    }
+}
