556a3ac16f2fbe5a69613edd5c50a0553ed3bd16
Phini0n <patrayowilson@gmail.com>
2024-10-10 16:22:56 -0400

    finished neighborhood-library main functions

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -12,49 +12,138 @@ public class NeighborhoodLibrary {
         inventory[3] = new Book("1338111043", "Tunnel of Bones (City of Ghosts)");
         inventory[4] = new Book("1538725428", "Cemetery Boys");
 
+        System.out.println("Hello, welcome to your neighborhood library!\n");
         while (true) {
-
-            System.out.print("Hello, welcome to your neighborhood library!\n\n" +
-                    "1) Show Available Books - Shows books in the library's inventory that can be checked out\n" +
-                    "2) Show Checked Out Books - Shows books checked out from the library's inventory\n" +
-                    "3) Exit - Exits the application\n" +
-                    "Enter: ");
+            System.out.print("""
+                    Main Menu
+                    1) Show Available Books - Shows books in the library's inventory that can be checked out
+                    2) Show Checked Out Books - Shows books checked out from the library's inventory
+                    X) Exit - Exits the application
+                    Enter:\s""");
 
             Scanner scanner = new Scanner(System.in);
-            int input = scanner.nextInt();
-            switch (input) {
+            String input = scanner.nextLine();
+            switch (input.toUpperCase()) {
                 // Show Available Books
-                case 1:
-                    listBooks(inventory);
+                case "1":
+                    listBooks(inventory, true, scanner);
                     continue;
                 // Show Checked Out Books
-                case 2:
+                case "2":
+                    listBooks(inventory, false, scanner);
                     continue;
                 // Exit
-                case 3:
+                case "X":
                     System.out.println("Goodbye!");
                     scanner.close();
                     break;
                 default:
-                    System.out.println("Invalid input!");
+                    System.out.println("\nInvalid input!\n");
                     continue;
             }
+            break;
         }
     }
 
-    public static void listBooks(Book[] books) {
+    public static void listBooks(Book[] books, boolean isCheckIn, Scanner scanner) {
         System.out.println("-----");
-        for (Book book : books) {
-            if (book != null && !book.isCheckedOut()) {
-                System.out.println(book);
-                try {
-                    Thread.sleep(500);
-                } catch (Exception e) {
-                    System.out.println(e);
+
+        //Prints Books that aren't checked out and begins check in process
+        if (isCheckIn) {
+            System.out.println("Available Books: ");
+            for (Book book : books) {
+                if (book != null && !book.isCheckedOut()) {
+                    System.out.println(book);
+                    try {
+                        Thread.sleep(500);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
                 }
             }
+
+            System.out.println("-----");
+
+            promptCheckInCheckout(books, isCheckIn, scanner);
         }
-        System.out.println("-----");
+        // Prints Books that are checked out and begins check out process
+        else {
+            System.out.println("Checked Out Books: ");
+            for (Book book : books) {
+                if (book != null && book.isCheckedOut()) {
+                    System.out.println(book);
+                    try {
+                        Thread.sleep(500);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                }
+            }
+
+            System.out.println("-----");
+
+            promptCheckInCheckout(books, isCheckIn, scanner);
+        }
     }
 
+    public static void promptCheckInCheckout(Book[] books, boolean isCheckIn, Scanner scanner) {
+        String operation = isCheckIn ? "check out" : "check in";
+        System.out.print("Would you like to " + operation + " a book?\n" +
+                "C) Continue\n" +
+                "X) Return to Main Menu\n" +
+                "Enter: ");
+        String s = scanner.nextLine();
+        switch (s.toUpperCase()) {
+            case "C" :
+                // If we're checking out, pass to checkOutBook Method.
+                if (isCheckIn) {
+                    checkOutBook(books, scanner);
+                // Else, we pass to checkInBook Method.
+                } else {
+                    checkInBook(books, scanner);
+                }
+            break;
+            case "X":
+                System.out.println("\nReturning to main menu...\n");
+                break;
+            default:
+                System.out.println("\nError, invalid input! Returning to Main Menu...\n");
+                break;
+        }
+    }
+
+    public static void checkOutBook(Book[] books, Scanner scanner) {
+        System.out.print("Enter your Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter a book ID: ");
+        String bookID = scanner.nextLine();
+
+        // Replaces leading zeros in ID and subtracts by 1 to get the inventory's index.
+        Book checkedOutBook = books[Integer.parseInt(bookID.replaceFirst("^0+(?!$)", "")) - 1];
+
+        // Checks if book entered is available to check out
+        if (checkedOutBook.isCheckedOut()) {
+            System.out.println("\nSorry, that book is not available.\n");
+        } else {
+            checkedOutBook.checkOut(name);
+            System.out.println("\n" + checkedOutBook.getTitle() + " is checked out!\n");
+        }
+    }
+
+    public static void checkInBook(Book[] books, Scanner scanner) {
+        System.out.print("Enter the book ID: ");
+        String bookID = scanner.nextLine();
+
+        // Replaces leading zeros in ID and subtracts by 1 to get the inventory's index.
+        Book checkedInBook = books[Integer.parseInt(bookID.replaceFirst("^0+(?!$)", "")) - 1];
+
+        // Checks if book entered is checked in or not
+        if (!checkedInBook.isCheckedOut())
+        {
+            System.out.println("\nThat book is not checked out!\n");
+        } else {
+            checkedInBook.checkIn();
+            System.out.println("\n" + checkedInBook.getTitle() + " is checked in!\n");
+        }
+    }
 }
